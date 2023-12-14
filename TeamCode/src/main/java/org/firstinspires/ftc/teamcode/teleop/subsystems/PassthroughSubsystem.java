598662a8e788f6ac100410d568086aea2ff0efb9
definitelynotvikram <113469040+definitelynotvikram@users.noreply.github.com>
2023-12-14 15:14:49 -0500

    It's about drive, it's about power

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/subsystems/PassthroughSubsystem.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.teleop.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+
+public class PassthroughSubsystem extends SubsystemBase {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/subsystems/PullupSubsystem.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.teleop.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+
+public class PullupSubsystem extends SubsystemBase {
+
+    public PullupSubsystem() {
+
+    }
+}
