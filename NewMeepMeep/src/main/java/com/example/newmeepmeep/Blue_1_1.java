38ede1c22860c07f186bd2331d46fd8870d79a1a
dreamfunicular <142117892+dreamfunicular@users.noreply.github.com>
2023-12-14 15:28:34 -0500

    Migrating MeepMeep to our new program

---
+++ NewMeepMeep/src/main/java/com/example/newmeepmeep/Blue_1_1.java
@@ -0,0 +1,34 @@
+package com.example.newmeepmeep;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+/**
+ * @author Anna Lynch
+ */
+
+
+// the buddy holly riff, in code form
+public class Blue_1_1 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-35.00, 63.00, 0))
+                                .lineToConstantHeading(new Vector2d(-35.00, 54.00))
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
